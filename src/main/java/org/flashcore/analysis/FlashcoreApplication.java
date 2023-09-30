559cb9532ffcea7d6540cd765d857c885c51b14f
Limpopoo93 <marachckow2013@yandex.ru>
2023-09-30 13:59:23 +0300

    init release-1.000.000

--- src/main/java/org/flashcore/analysis/FlashcoreApplication.java
+++ src/main/java/org/flashcore/analysis/FlashcoreApplication.java
@@ -1,7 +1,9 @@
 package org.flashcore.analysis;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class FlashcoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(FlashcoreApplication.class, args);
