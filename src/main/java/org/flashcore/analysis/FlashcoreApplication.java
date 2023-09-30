e85ff514d2416b020f5d503eae5cb72777280418
Limpopoo93 <marachckow2013@yandex.ru>
2023-09-30 13:40:14 +0300

    init

---
+++ src/main/java/org/flashcore/analysis/FlashcoreApplication.java
@@ -0,0 +1,9 @@
+package org.flashcore.analysis;
+
+import org.springframework.boot.SpringApplication;
+
+public class FlashcoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FlashcoreApplication.class, args);
+    }
+}
